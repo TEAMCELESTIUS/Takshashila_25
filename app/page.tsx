c29a53bb9cd9a046db7c3e8299e1fbcdd29a69c6
KRISHNASAKTHIESWAR <ekrishnasakthi@gmail.com>
2025-02-09 19:13:13 +0530

    (Krishna) feature : Added custom cursor

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import AboutUs from '../components/aboutUs';
 import NavBar from '../components/navBar';
-import SponsorSlider from '../components/sponsorSlider';
+import SponsorSlider from '../components/SponsorSlider';
 import Footer from '../components/footer';
 import LocomotiveScrollProvider from '@/components/locomotiveScroll';
-
+import InteractiveCursor from '@/components/interactiveCursor';
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="cursor-none">
+      <InteractiveCursor />
       <LocomotiveScrollProvider>
-      
         <NavBar />
         <AboutUs />
         <SponsorSlider/>
