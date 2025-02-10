64c48a6957b0532692d02ea9229199739e4aa6e4
Chorko <chorkoc.cse2023@citchennai.net>
2025-02-11 00:54:26 +0530

    [Feature component] Initial commit

---
+++ .next/static/chunks/app/layout-5b5e295e50ceea76.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{4552:function(n,e,u){Promise.resolve().then(u.t.bind(u,2205,23))},2205:function(){}},function(n){n.O(0,[918,42,516,744],function(){return n(n.s=4552)}),_N_E=n.O()}]);
